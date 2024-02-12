29df07ee3a17459aec8616c025b624a338a05a2e
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-02-13 01:56:02 +0200

    Triangle area calculated

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-console.log("connected");
+function calculateTriangleArea() {
+  const base = getInputValue("triangleBase");
+  const height = getInputValue("triangleHeight");
+  const area = 0.5 * base * height;
+  setArea("triangleArea", area);
+  console.log(area);
+}
+
+function getInputValue(idName) {
+  const value = parseFloat(document.getElementById(idName).value);
+  return value;
+}
+function setArea(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
+}
